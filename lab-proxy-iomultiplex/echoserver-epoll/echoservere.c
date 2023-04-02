951010b64ef41d83633bc4b29be78ef14947dc99
Casey Deccio <casey@deccio.net>
2023-04-01 23:55:51 -0600

    Update echoservere.c

--- lab-proxy-iomultiplex/echoserver-epoll/echoservere.c
+++ lab-proxy-iomultiplex/echoserver-epoll/echoservere.c
@@ -16,23 +16,29 @@
 
 struct client_info {
 	int fd;
+	int total_length;
 	char desc[1024];
 };
 
 int main(int argc, char **argv) 
 {
-	struct addrinfo hints;
-	struct addrinfo *result, *rp;
-	int sfd, s, connfd;
-	int af;
 	int portindex;
+	unsigned short port;
+	int address_family;
+	int sock_type;
+	struct sockaddr_in local_addr_in;
+	struct sockaddr_in6 local_addr_in6;
 
-	socklen_t clientlen;
-	struct sockaddr_storage clientaddr;
+	int sfd;
+	struct sockaddr_storage *remote_addr;
+	struct sockaddr *local_addr;
+	socklen_t local_addr_len, remote_addr_len;
+
+	ssize_t nread;
 
 	int efd;
 	struct epoll_event event;
-	struct epoll_event *events;
+	struct epoll_event events[MAXEVENTS];
 	int i;
 	int len;
 
@@ -40,9 +46,12 @@ int main(int argc, char **argv)
 	struct client_info *listener;
 	struct client_info *active_client;
 
+	int connfd;
+
 	size_t n; 
 	char buf[MAXLINE]; 
 
+	/* Check usage */
 	if (!(argc == 2 || (argc == 3 &&
 			(strcmp(argv[1], "-4") == 0 || strcmp(argv[1], "-6") == 0)))) {
 		fprintf(stderr, "Usage: %s [ -4 | -6 ] port\n", argv[0]);
@@ -53,50 +62,43 @@ int main(int argc, char **argv)
 	} else {
 		portindex = 2;
 	}
-	/* Use IPv4 by default (or if -4 is used).  If IPv6 is specified,
-	 * then use that instead. */
+
+	/* Use IPv4 by default (or if -4 is specified);
+	 * If -6 is specified, then use IPv6 instead. */
 	if (argc == 2 || strcmp(argv[portindex], "-4") == 0) {
-		af = AF_INET;
+		address_family = AF_INET;
 	} else {
-		af = AF_INET6;
+		address_family = AF_INET6;
 	}
 
-	/* pre-socket setup; getaddrinfo() */
+	port = atoi(argv[portindex]);
+	sock_type = SOCK_STREAM;
 
-	memset(&hints, 0, sizeof(struct addrinfo));
+	if (address_family == AF_INET) {
+		local_addr_in.sin_family = address_family;
+		local_addr_in.sin_addr.s_addr = INADDR_ANY; // listen on any/all IPv4 addresses
+		local_addr_in.sin_port = htons(port);       // specify port explicitly, in network byte order
 
-	/* As a server, we want to exercise control over which protocol (IPv4
-	   or IPv6) is being used, so we specify AF_INET or AF_INET6 explicitly
-	   in hints, depending on what is passed on on the command line. */
-	hints.ai_family = af;	/* Choose IPv4 or IPv6 */
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE;	/* For wildcard IP address */
-	hints.ai_protocol = 0;		  /* Any protocol */
-	hints.ai_canonname = NULL;
-	hints.ai_addr = NULL;
-	hints.ai_next = NULL;
+		// Assign local_addr and local_addr_len to ipv4addr
+		local_addr = (struct sockaddr *)&local_addr_in;
+		local_addr_len = sizeof(local_addr_in);
+	} else { // address_family == AF_INET6
+		local_addr_in6.sin6_family = address_family;
+		local_addr_in6.sin6_addr = in6addr_any;     // listen on any/all IPv6 addresses
+		local_addr_in6.sin6_port = htons(port);     // specify port explicitly, in network byte order
 
-	s = getaddrinfo(NULL, argv[portindex], &hints, &result);
-	if (s != 0) {
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
-		exit(EXIT_FAILURE);
+		// Assign local_addr and local_addr_len to ipv6addr
+		local_addr = (struct sockaddr *)&local_addr_in6;
+		local_addr_len = sizeof(local_addr_in6);
 	}
 
-	/* getaddrinfo() returns a list of address structures.  However,
-	   because we have only specified a single address family (AF_INET or
-	   AF_INET6) and have only specified the wildcard IP address, there is
-	   no need to loop; we just grab the first item in the list. */
-	if ((s = getaddrinfo(NULL, argv[portindex], &hints, &result)) < 0) {
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
-		exit(EXIT_FAILURE);
-	}
 
-	if ((sfd = socket(result->ai_family, result->ai_socktype, 0)) < 0) {
+	if ((sfd = socket(address_family, sock_type, 0)) < 0) {
 		perror("Error creating socket");
 		exit(EXIT_FAILURE);
 	}
 
-	if (bind(sfd, result->ai_addr, result->ai_addrlen) < 0) {
+	if (bind(sfd, local_addr, local_addr_len) < 0) {
 		perror("Could not bind");
 		exit(EXIT_FAILURE);
 	}
@@ -106,18 +108,15 @@ int main(int argc, char **argv)
 		exit(EXIT_FAILURE);
 	}
 
-	freeaddrinfo(result);   /* No longer needed */
-
-	if ((efd = epoll_create1(0)) < 0) {
-		fprintf(stderr, "error creating epoll fd\n");
-		exit(1);
-	}
-
-
 	// set listening file descriptor non-blocking
 	if (fcntl(sfd, F_SETFL, fcntl(sfd, F_GETFL, 0) | O_NONBLOCK) < 0) {
 		fprintf(stderr, "error setting socket option\n");
 		exit(1);
+	}
+
+	if ((efd = epoll_create1(0)) < 0) {
+		perror("Error with epoll_create1");
+		exit(EXIT_FAILURE);
 	}
 
 	// allocate memory for a new struct client_info, and populate it with
@@ -132,11 +131,8 @@ int main(int argc, char **argv)
 	event.events = EPOLLIN | EPOLLET;
 	if (epoll_ctl(efd, EPOLL_CTL_ADD, sfd, &event) < 0) {
 		fprintf(stderr, "error adding event\n");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
-
-	/* Buffer where events are returned */
-	events = calloc(MAXEVENTS, sizeof(struct epoll_event));
 
 	while (1) {
 		// wait for event to happen (-1 == no timeout)
@@ -147,7 +143,7 @@ int main(int argc, char **argv)
 			// (appropriately) to a struct client_info *.
 			active_client = (struct client_info *)(events[i].data.ptr);
 
-			printf("New event for %s\n", active_client->desc);
+			printf("New event for fd %d (%s)\n", active_client->fd, active_client->desc);
 
 			if ((events[i].events & EPOLLERR) ||
 					(events[i].events & EPOLLHUP) ||
@@ -160,11 +156,10 @@ int main(int argc, char **argv)
 			}
 
 			if (sfd == active_client->fd) {
-				clientlen = sizeof(struct sockaddr_storage); 
-
 				// loop until all pending clients have been accepted
 				while (1) {
-					connfd = accept(active_client->fd, (struct sockaddr *)&clientaddr, &clientlen);
+					remote_addr_len = sizeof(struct sockaddr_storage);
+					connfd = accept(active_client->fd, (struct sockaddr *)&remote_addr, &remote_addr_len);
 
 					if (connfd < 0) {
 						if (errno == EWOULDBLOCK ||
@@ -188,6 +183,7 @@ int main(int argc, char **argv)
 					// info for the new client
 					new_client = (struct client_info *)malloc(sizeof(struct client_info));
 					new_client->fd = connfd;
+					new_client->total_length = 0;
 					sprintf(new_client->desc, "Client with file descriptor %d", connfd);
 
 					// register the client file descriptor
@@ -205,7 +201,7 @@ int main(int argc, char **argv)
 				// has closed the connection or (2) there is no
 				// data left to be read.
 				while (1) {
-					len = recv(active_client->fd, buf, MAXLINE, 0);   
+					len = recv(active_client->fd, buf, MAXLINE, 0);
 					if (len == 0) { // EOF received
 						// closing the fd will automatically
 						// unregister the fd from the efd
@@ -223,13 +219,13 @@ int main(int argc, char **argv)
 						}
 						break;
 					} else {
-						printf("Received %d bytes\n", len);
+						active_client->total_length += len;
+						printf("Received %d bytes (total: %d)\n", len, active_client->total_length);
 						send(active_client->fd, buf, len, 0);
 					}
 				}
 			}
 		}
 	}
-	free(events);
 	free(listener);
 }
